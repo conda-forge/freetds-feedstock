134a33e9dd712e15380c7cb7d97b6f6454b92085
Marius Van Niekerk <marius.v.niekerk@gmail.com>
2017-10-26 12:49:09 -0400

    Attempt to get windows builds going

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,13 @@
 
-set OPENSSL_ROOT_DIR=%LIBRARY_PREFIX%
+set OPENSSL_ROOT_DIR=%PREFIX%
 
 set CMAKE_GENERATOR="NMake Makefiles JOM"
 mkdir build
 cd build
 
 cmake -G "%CMAKE_GENERATOR%" ^
-         -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-         -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+         -DCMAKE_PREFIX_PATH=%PREFIX% ^
+         -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
          -DCMAKE_BUILD_TYPE=Release ^
          -DBUILD_SHARED_LIBS=True ..
 if errorlevel 1 exit 1
