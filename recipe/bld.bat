f7bc9377b15fb568ea5c4ecd10642b5043f83d51
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-10-27 09:46:32 -0400

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ set CMAKE_GENERATOR="NMake Makefiles JOM"
 mkdir build
 cd build
 
-cmake -G "%CMAKE_GENERATOR%" ^
+cmake -G %CMAKE_GENERATOR% ^
          -DCMAKE_PREFIX_PATH=%PREFIX% ^
          -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
          -DCMAKE_BUILD_TYPE=Release ^
