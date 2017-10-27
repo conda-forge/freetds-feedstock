5de2027e588c305cdc2564ee95969f260b086ec7
Marius van Niekerk <marius.v.niekerk@gmail.com>
2017-10-27 19:03:23 -0400

    Once more

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,8 +12,5 @@ cmake -G %CMAKE_GENERATOR% ^
          -DBUILD_SHARED_LIBS=True ..
 if errorlevel 1 exit 1
 
-cmake --build . --config Release --target all
-if errorlevel 1 exit 1
-
-cmake --build . --config Release --target install
+cmake --build . --target INSTALL --config Release
 if errorlevel 1 exit 1
