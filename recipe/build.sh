1e7510c9080de84343a2bfd952abc6ab93e3782b
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-03-26 00:22:01 -0500

    Make recipe cross compile capable; CB3ize

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-./configure --prefix $PREFIX --with-unixodbc=$PREFIX --with-openssl=$PREFIX
+./configure \
+  --prefix=$PREFIX \
+  --with-unixodbc=$PREFIX \
+  --with-openssl=$PREFIX \
+  --host=$HOST \
+  --build=$BUILD
 make
 # To run this check we need to have access to a mssql instance.
 # make check
