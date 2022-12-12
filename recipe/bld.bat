25429f8f6c17309e0fa7db0035a44f8be31c120e
PrimozGodec <p.godec9@gmail.com>
2022-12-12 13:45:42 +0100

    Update recipes for 1.3.9

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,10 @@
-
 set OPENSSL_ROOT_DIR=%LIBRARY_PREFIX%
 
+set "CMAKE_GENERATOR=NMake Makefiles JOM"
 mkdir build
 cd build
 
-cmake -GNinja ^
+cmake -G "%CMAKE_GENERATOR%" ^
          -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
          -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
          -DCMAKE_BUILD_TYPE=Release ^
